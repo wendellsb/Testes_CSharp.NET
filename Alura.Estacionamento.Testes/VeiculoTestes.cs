e6195c3d19af1a3caa8f7055f9cae69c827d855c
Wendell Santos <88899145+wendellsb@users.noreply.github.com>
2022-05-24 15:38:42 -0300

    Initial commit

---
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,37 @@
+using Alura.Estacionamento.Modelos;
+using System;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            // Padrão AAA
+            //Arrange - preparação de cenário
+            var veiculo = new Veiculo();
+
+            //Act - metodo a ser testado
+            veiculo.Acelerar(10);
+
+            //Assert - resultado obtido a partir de validações com o resultado esperado de acordo com o metodo
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestarVeiculoFrear()
+        {
+            // Padrão AAA
+            //Arrange - preparação de cenário
+            var veiculo = new Veiculo();
+
+            //Act - metodo a ser testado
+            veiculo.Frear(10);
+
+            //Assert - resultado obtido a partir de validações com o resultado esperado de acordo com o metodo
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
